a1043468af7e06fc0a0c20fb863424889ae8082a
rifai1010 <2017rifai2017@gmail.com>
2024-12-27 01:22:32 +0700

    meletup

--- api/index.php
+++ api/index.php
@@ -3,13 +3,3 @@
 require __DIR__.'/../public/index.php';
 
 $app = require_once __DIR__.'/../bootstrap/app.php';
-
-$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
-
-$response = $kernel->handle(
-    $request = Illuminate\Http\Request::capture()
-);
-
-$response->send();
-
-$kernel->terminate($request, $response);
